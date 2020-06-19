0da084b47373c3df90d951e2c747273500ba0959
jasonj85 <jasonj85@gmail.com>
2020-06-19 08:42:38 +0100

    Demo version of app using cookies and session data saved in MongoDB

---
+++ session-cookies/controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login',
+    isAuthenticated: req.session.isLoggedIn
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5ee3323d9e1c863a6cafe63f')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
